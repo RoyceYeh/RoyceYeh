c8c6f89f6045fc5d3a483e8e10d3ab5807e0ccc0
RoyceYeh <royz.reus@gmail.com>
2022-01-07 20:10:28 +0800

    setapllo

--- apollo.config.js
+++ apollo.config.js
@@ -1,12 +1,12 @@
 // apollo.config.js
-module.exports = {
-	client: {
-		service: {
-			name: "todolist",
-			// GraphQL API 的 URL
-			url: "http://35.189.161.175:8080/v1/graphql",
-		},
-		// 通过扩展名选择需要处理的文件
-		includes: ["src/App.vue", "src/main.js"],
-	},
-};
+// module.exports = {
+// 	client: {
+// 		service: {
+// 			name: "todolist",
+// 			// GraphQL API 的 URL
+// 			url: "http://35.189.161.175:8080/v1/graphql",
+// 		},
+// 		// 通过扩展名选择需要处理的文件
+// 		includes: ["src/App.vue", "src/main.js"],
+// 	},
+// };
--- src/main.js
+++ src/main.js
@@ -1,35 +1,86 @@
-//import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+// import { createApp } from "vue";
+// import App from "./App.vue";
+// import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
-import { ApolloClient } from "apollo-client";
-import { createHttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
+// import { ApolloClient } from "apollo-client";
+// import { createHttpLink } from "apollo-link-http";
+// import { InMemoryCache } from "apollo-cache-inmemory";
+
+// import Vue from "vue";
+// import VueApollo from "vue-apollo";
+
+// Vue.use(VueApollo);
+
+// 與 API 的 HTTP 連接
+// const httpLink = createHttpLink({
+// 	uri: "http://localhost:3020/graphql",
+// });
+
+// 暫存
+// const cache = new InMemoryCache();
+
+// const apolloClient = new ApolloClient({
+// 	link: httpLink,
+// 	cache,
+// });
+
+// const apolloProvider = new VueApollo({
+// 	defaultClient: apolloClient,
+// });
+
+//  import ApolloClient from "apollo-client";
+//  import { HttpLink } from "apollo-link-http";
+//  import { InMemoryCache } from "apollo-cache-inmemory";
+
+//createApp(App).use(apolloProvider.provider).mount("#app");
+//createApp(App).use(router).mount("#app");
 
 import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import AuthPlugin from "./plugins/auth";
+
+import ApolloClient from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
 import VueApollo from "vue-apollo";
+Vue.use(AuthPlugin);
 Vue.use(VueApollo);
-// 与 API 的 HTTP 连接
-const httpLink = createHttpLink({
-	// 你需要在这里使用绝对路径
+Vue.config.productionTip = false;
+const getHeaders = () => {
+	const headers = {};
+	const token = window.localStorage.getItem("apollo-token");
+	if (token) {
+		headers.authorization = `Bearer ${token}`;
+	}
+	return headers;
+};
+
+// Create an http link:
+const link = new HttpLink({
 	uri: "http://35.189.161.175:8080/v1/graphql",
+	fetch,
+	headers: getHeaders(),
 });
 
-// 缓存实现
-const cache = new InMemoryCache();
-
-// 创建 apollo 客户端
-const apolloClient = new ApolloClient({
-	link: httpLink,
-	cache,
+const client = new ApolloClient({
+	link: link,
+	cache: new InMemoryCache({
+		addTypename: true,
+	}),
 });
-
 const apolloProvider = new VueApollo({
-	defaultClient: apolloClient,
+	defaultClient: client,
 });
 
+const app = Vue.createApp({
+	router,
+	apolloProvider,
+	render: (h) => h(App),
+});
 new Vue({
 	router,
 	apolloProvider,
